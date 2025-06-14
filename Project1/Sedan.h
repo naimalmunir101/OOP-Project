5b20f5d3a6e56bac98fa33a58838014687333327
naimalmunir101 <bscs24101@itu.edu.pk>
2025-06-14 15:08:36 +0500

    Sedan class added

--- Project1/Sedan.h
+++ Project1/Sedan.h
@@ -1,1 +1,37 @@
-#pragma once
+#ifndef SEDAN_H
+#define SEDAN_H
+
+#include "SportsCar.h"
+
+class Sedan : public Vehicle
+{
+protected:
+    int airBags;
+    string powerLocks;
+    string powerWindows;
+public:
+    Sedan()
+    {
+        airBags = 0;
+        powerLocks = "";
+        powerWindows = "";
+        vehicleType = "Sedan";
+    }
+
+    void vehicleReturn();
+    Sedan dataReading(string temp);
+    void dataRecord();
+
+    friend void removeSedan(string, string, int);
+    friend void sedanEdit(Sedan*, int, int);
+
+
+    friend istream& operator >> (istream&, Sedan&);
+    friend ostream& operator << (ostream&, const Sedan&);
+
+};
+
+
+
+
+#endif 
