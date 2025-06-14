e570aa5e5da2734e38c3d5971a152d963dc81cf7
naimalmunir101 <bscs24101@itu.edu.pk>
2025-06-14 15:16:46 +0500

    Edited Employee class

--- Project1/Employee.h
+++ Project1/Employee.h
@@ -1,1 +1,33 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include "Person.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee : public Person
+{
+    double salary;
+
+public:
+    Employee()
+    {
+        salary = 0;
+    }
+
+    void writeToFile();
+    friend void readFromFileEmployee();
+    friend Employee searchEmployee();
+    friend void removeEmployee(int mode);
+    friend void employeeEdit(Employee*, int, int);
+
+    friend istream& operator >> (istream& input, Employee& person);
+    friend ostream& operator << (ostream& output, const Employee& person);
+};
+
+void readFromFileEmployee();
+
+#endif 
+
 
