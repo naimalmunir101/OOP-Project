31ad9c41ebebbc8a6a053f29ed98bb27cf457824
naimalmunir101 <bscs24101@itu.edu.pk>
2025-06-14 15:05:22 +0500

    edited SportsCar class

--- Project1/SportsCar.h
+++ Project1/SportsCar.h
@@ -1,1 +1,33 @@
-#pragma once
+#ifndef SPORTSCAR_H
+#define SPORTSCAR_H
+
+#include "Vehicle.h"
+
+class SportsCar :public Vehicle
+{
+    string turboType;
+    string spoilerType;
+public:
+    SportsCar()
+    {
+        turboType = "";
+        spoilerType = "";
+        vehicleType = "SportsCar";
+    }
+
+    void vehicleReturn();
+
+    void dataRecord();
+
+    SportsCar dataReading(string temp);
+
+    friend void removeSportsCar(string regNum, string numPlateChange, int);
+    friend void sportsCarEdit(SportsCar*, int, int);
+
+    friend ostream& operator << (ostream&, const SportsCar&);
+    friend istream& operator >> (istream&, SportsCar&);
+
+};
+
+
+#endif 
