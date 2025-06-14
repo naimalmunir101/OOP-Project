e1272ad77fd171624d1e46aeee3225160d4dd0d5
naimalmunir101 <bscs24101@itu.edu.pk>
2025-06-14 15:21:29 +0500

    classes edited

--- Project1/Vehicle.h
+++ Project1/Vehicle.h
@@ -1,1 +1,47 @@
-#pragma once
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include<iostream>
+#include <fstream>
+#include "Customer.h"
+using namespace std;
+
+class Vehicle
+{
+protected:
+    int noOfDoors;
+    int noOfTyres;
+    int noOfSeats;
+    int engineCC;
+
+    string numPlate;
+    string color;
+    string transmissionType;
+    string fault;
+    string vehicleType;
+public:
+    Vehicle()
+    {
+        noOfDoors = 0;
+        noOfSeats = 0;
+        noOfTyres = 0;
+        engineCC = 0;
+        numPlate = "";
+        color = "";
+        transmissionType = "";
+        fault = "";
+        vehicleType = "";
+    }
+
+    string setNumPlate(string);
+
+    void generalInput();
+    void generalOutput() const;
+
+    virtual void vehicleReturn() = 0;//ye polymorphism k liye hai sab apni spni class m is ka
+    //function bananah. Refrence k liye SportsCar dekh lena
+
+    string getVehicleType();
+};
+
+#endif
